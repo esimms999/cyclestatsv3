4b142cd223ee6c33143e465482b29b3e9472774e
esimms999 <esimms@yahoo.com>
2026-08-28 10:40:37 +0000

    Docker fix

--- R/app_config.R
+++ R/app_config.R
@@ -1,5 +1,5 @@
 # Helper to resolve paths within the installed package.
-.app_pkg <- "cyclestats"
+.app_pkg <- "cyclestatsv3"
 app_sys <- function(...) {
   system.file(..., package = .app_pkg)
 }
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,3 @@
 library(testthat)
 
-test_check("cyclestats")
+test_check("cyclestatsv3")
